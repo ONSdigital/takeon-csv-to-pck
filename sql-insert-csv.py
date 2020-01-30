dd4c14045910385a1c690c61249c3d4ae1c1deb0
RyanAldy <ryanalderson91@gmail.com>
2020-01-30 09:32:01 +0000

    Replaced all double quotes with single quotes

--- sql-insert-csv.py
+++ sql-insert-csv.py
@@ -38,19 +38,19 @@ insert_statement = """INSERT INTO dev01.contributor (
 insert_row = ''
 
 for row in dictionary_list:
-    insert_row += "(" + esc + row['responder_id'] + esc + "," + esc + row['period'] + esc 
-    insert_row += "," + esc + survey + esc + "," + str(form_id) + "," + esc + status + esc + "," + "now()"
-    insert_row += "," + esc + ' ' + esc + ',' + esc + ' ' + esc
-    insert_row += "," + esc + '0004' + esc + "," + esc + 'T' + esc + "," + esc + '70110' + esc + "," + esc + '70110' + esc + "," + esc + '41100' + esc
-    insert_row += "," + esc + '41100' + esc + "," + str(750) + "," + str(748) + "," + str(100) + "," + str(100) + "," + str(100) + "," + str(100) + "," + str(99999)
-    insert_row += "," + str(99999) + "," + row['enterprise_ref'] + "," + esc + '2906948169' + esc + "," + str(1) + "," + esc + 'S' + esc 
-    insert_row += "," + esc + '326935020502' + esc + "," + esc + '2135632144542' + esc
-    insert_row += "," + esc + '97395797' + esc + "," + str(178) + "," + str(0) + "," + str(0) + "," + esc + '1' + esc + "," + esc + 'E' + esc 
-    insert_row += "," + esc + row['gor_code'] + esc + "," + esc + '06/06/1975' + esc
-    insert_row += "," + esc + 'Hayes, Fletcher and Shaw' + esc + "," + esc + 'Lawrence Group Ltd' + esc + "," + esc + '9 Brenda falls,East Harry, Wales' + esc 
-    insert_row += "," + esc + 'L2T 0PR' + esc + "," + esc + 'Sole Trader' + esc + "," + esc + 'Heather Griffiths' + esc
-    insert_row += "," + esc + '01633 5551234' + esc + "," + esc + '54321' + esc + "," + esc + 'P' + esc + "," + esc + ' ' + esc
-    insert_row += "," + esc + 'fisdba' + esc + "," + 'now()' + "," + esc + 'fisdba' + esc + "," + 'now()' + '),' + '\n'
+    insert_row += '(' + esc + row['responder_id'] + esc + ',' + esc + row['period'] + esc 
+    insert_row += ',' + esc + survey + esc + ',' + str(form_id) + ',' + esc + status + esc + ',' + 'now()'
+    insert_row += ',' + esc + ' ' + esc + ',' + esc + ' ' + esc
+    insert_row += ',' + esc + '0004' + esc + ',' + esc + 'T' + esc + ',' + esc + '70110' + esc + ',' + esc + '70110' + esc + ',' + esc + '41100' + esc
+    insert_row += ',' + esc + '41100' + esc + ',' + str(750) + ',' + str(748) + ',' + str(100) + ',' + str(100) + ',' + str(100) + ',' + str(100) + ',' + str(99999)
+    insert_row += ',' + str(99999) + ',' + row['enterprise_ref'] + ',' + esc + '2906948169' + esc + ',' + str(1) + ',' + esc + 'S' + esc 
+    insert_row += ',' + esc + '326935020502' + esc + ',' + esc + '2135632144542' + esc
+    insert_row += ',' + esc + '97395797' + esc + ',' + str(178) + ',' + str(0) + ',' + str(0) + ',' + esc + '1' + esc + ',' + esc + 'E' + esc 
+    insert_row += ',' + esc + row['gor_code'] + esc + ',' + esc + '06/06/1975' + esc
+    insert_row += ',' + esc + 'Hayes, Fletcher and Shaw' + esc + ',' + esc + 'Lawrence Group Ltd' + esc + ',' + esc + '9 Brenda falls,East Harry, Wales' + esc 
+    insert_row += ',' + esc + 'L2T 0PR' + esc + ',' + esc + 'Sole Trader' + esc + ',' + esc + 'Heather Griffiths' + esc
+    insert_row += ',' + esc + '01633 5551234' + esc + ',' + esc + '54321' + esc + ',' + esc + 'P' + esc + ',' + esc + ' ' + esc
+    insert_row += ',' + esc + 'fisdba' + esc + ',' + 'now()' + ',' + esc + 'fisdba' + esc + ',' + 'now()' + '),' + '\n'
     
-sql_file = open("insert-into-contributor.sql", "w")
+sql_file = open('insert-into-contributor.sql', 'w')
 sql_file.writelines([insert_statement, insert_row[:-2], ';'])
